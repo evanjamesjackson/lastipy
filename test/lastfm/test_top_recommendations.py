dc811941557ce1a9be051743acab6411c6a11629
Evan Jackson <evanjamesjackson@gmail.com>
2019-04-16 19:45:18 -0300

    Cleaned up duplication

--- test/lastfm/test_top_recommendations.py
+++ test/lastfm/test_top_recommendations.py
@@ -11,14 +11,15 @@ from unittest.mock import patch
 class TopRecommendationsFetcherTest(unittest.TestCase):
     def test_recent_tracks_are_filtered(self):
         recent_fetcher = RecentTracksFetcher()
-        recent_fetcher.fetch = MagicMock(return_value=[Track(track_name="While My Guitar Gently Weeps", artist="The Beatles")])
+        recent_track = Track(track_name="While My Guitar Gently Weeps", artist="The Beatles")
+        recent_fetcher.fetch = MagicMock(return_value=[recent_track])
 
         top_fetcher = TopTracksFetcher()
         top_fetcher.fetch = MagicMock(return_value=[Track(track_name="Badge", artist="Cream")])
 
         similar_fetcher = SimilarTracksFetcher()
         expected_recommendation = Track(track_name="White Room", artist="Cream")
-        similar_fetcher.fetch = MagicMock(return_value=[Track(track_name="While My Guitar Gently Weeps", artist="The Beatles"),
+        similar_fetcher.fetch = MagicMock(return_value=[recent_track,
                                                         expected_recommendation])
 
         recommendations = TopRecommendationsFetcher(similar_fetcher=similar_fetcher, top_fetcher=top_fetcher, recent_fetcher=recent_fetcher).fetch(user="meeee")
