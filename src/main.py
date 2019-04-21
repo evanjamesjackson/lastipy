fcee0d323d57bded209369ef09483ac25fce4f87
Evan Jackson <evanjamesjackson@gmail.com>
2019-04-20 22:22:32 -0300

    Imports

--- src/main.py
+++ src/main.py
@@ -4,9 +4,7 @@ from src.lastfm.top_tracks import TopTracksFetcher
 from src.lastfm.similar_tracks import SimilarTracksFetcher
 from src.lastfm.top_recommendations import TopRecommendationsFetcher
 from src.lastfm.recent_tracks import RecentTracksFetcher
-from src.spotify.playlist import *
-from src.spotify.search import *
-from src.spotify.library import *
+from src.spotify import library, playlist, search
 from random import shuffle
 
 
@@ -33,16 +31,16 @@ def main():
 
     track_ids = []
     for track in recommendations[:playlist_size]:
-        search_results = search(username=spotify_user, query=track.artist + " " + track.track_name)
+        search_results = search.search(username=spotify_user, query=track.artist + " " + track.track_name)
         if search_results['tracks']['items']:
             track_ids.append(search_results['tracks']['items'][0]['id'])
 
-    saved_tracks = get_saved_tracks(spotify_user)
-    playlist_tracks = get_tracks_in_playlists(spotify_user)
+    saved_tracks = library.get_saved_tracks('sonofjack3')
+    playlist_tracks = library.get_tracks_in_playlists(spotify_user)
 
     track_ids = [track_id for track_id in track_ids if track_id not in saved_tracks and track_id not in playlist_tracks]
 
-    add_to_playlist(spotify_user, playlist_name, track_ids)
+    playlist.add_to_playlist(spotify_user, playlist_name, track_ids)
 
 
 if __name__ == "__main__":
