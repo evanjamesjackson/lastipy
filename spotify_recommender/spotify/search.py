eb7fb47159603925f017b42dc9f29f360e300da2
Evan Jackson <evanjamesjackson@gmail.com>
2019-04-22 13:21:20 -0300

    Now returning track IDs rather than entire json objects

--- spotify_recommender/spotify/search.py
+++ spotify_recommender/spotify/search.py
@@ -4,9 +4,18 @@ from .token import *
 import json
 
 
-def search(username, query):
+def search_for_tracks(username, query):
+    """Returns a list of track IDs for the given query"""
+
     token = get_token(username)
     spotify = spotipy.Spotify(auth=token)
     results = spotify.search(q=query)
     logging.debug("Search results for query " + query + ": " + json.dumps(results))
-    return results
+    return _extract_ids(results)
+
+
+def _extract_ids(results):
+    track_ids = []
+    if results['tracks']['items']:
+        track_ids.append(results['tracks']['items'][0]['id'])
+    return track_ids
