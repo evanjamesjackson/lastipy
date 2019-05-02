d3371d6a53f1ae92057411f32b1f1cc885dbc800
Evan Jackson <evanjamesjackson@gmail.com>
2019-05-02 19:57:42 -0300

    Added spotify_id field to Track object

--- spotify_recommender/track.py
+++ spotify_recommender/track.py
@@ -1,12 +1,16 @@
 class Track:
     """Represents a track"""
 
-    def __init__(self, track_name, artist):
+    def __init__(self, track_name, artist, spotify_id=None):
         self.track_name = track_name
         self.artist = artist
+        self.spotify_id = spotify_id
 
     def __eq__(self, other):
-        return isinstance(other, Track) and self.track_name == other.track_name and self.artist == other.artist
+        return isinstance(other, Track) \
+               and self.track_name == other.track_name \
+               and self.artist == other.artist \
+               and self.spotify_id == other.spotify_id
 
     def __repr__(self):
         return str(self.__dict__)
