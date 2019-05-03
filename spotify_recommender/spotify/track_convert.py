2b17f861b27ae87610aec3575a67ca784801fce3
Evan Jackson <evanjamesjackson@gmail.com>
2019-05-02 21:28:03 -0300

    Added track_convert.py to spotify package

---
+++ spotify_recommender/spotify/track_convert.py
@@ -0,0 +1,16 @@
+from ..track import Track
+
+
+def convert_json_tracks(json_tracks):
+    return [convert_json_track(json_track) for json_track in json_tracks]
+
+
+def convert_json_track(json_track):
+    # Just getting the first artist, even if there's multiple
+    if 'track' in json_track:
+        # Some endpoints do this, others don't
+        json_track = json_track['track']
+    artist = json_track['artists'][0]['name']
+    name = json_track['name']
+    track_id = json_track['id']
+    return Track(track_name=name, artist=artist, spotify_id=track_id)
