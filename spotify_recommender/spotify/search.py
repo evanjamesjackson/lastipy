df7598ce3d6c03609cb3045ccbbbfc26498698c2
Evan Jackson <evanjamesjackson@gmail.com>
2019-05-02 21:55:47 -0300

    Debug statement for search results

--- spotify_recommender/spotify/search.py
+++ spotify_recommender/spotify/search.py
@@ -2,6 +2,7 @@ import logging
 import spotipy
 from . import token
 from . import track_convert
+import json
 
 
 def search_for_tracks(username, query):
@@ -9,6 +10,8 @@ def search_for_tracks(username, query):
 
     spotify = spotipy.Spotify(auth=token.get_token(username))
     results = spotify.search(q=query)
+    logging.debug("All search results for query " + query + ": " + json.dumps(results))
+
     converted_tracks = []
     if results['tracks']:
         converted_tracks = track_convert.convert_json_tracks(results['tracks']['items'])
