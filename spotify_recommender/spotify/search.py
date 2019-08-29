6259c76348e768e418478ba1acbc8e983c66adae
Evan Jackson <evanjamesjackson@gmail.com>
2019-08-28 21:42:27 -0300

    Fixed comment talking about IDs

--- spotify_recommender/spotify/search.py
+++ spotify_recommender/spotify/search.py
@@ -6,7 +6,7 @@ import json
 
 
 def search_for_tracks(username, query):
-    """Returns a list of track IDs for the given query"""
+    """Returns a list of tracks for the given query"""
 
     spotify = spotipy.Spotify(auth=token.get_token(username))
     results = spotify.search(q=query)
