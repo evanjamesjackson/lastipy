f6c1ea810a91a86fffd290c7d3b0c342d1d443af
Evan Jackson <evanjamesjackson@gmail.com>
2019-10-04 22:30:07 -0300

    Removed unnecessary patching

--- test/lastfm/test_recommendation_calculator.py
+++ test/lastfm/test_recommendation_calculator.py
@@ -2,12 +2,10 @@ import unittest
 from src.lastfm.scrobbled_track import ScrobbledTrack
 from src.lastfm.recommended_track import RecommendedTrack
 from src.lastfm import recommendation_calculator
-from unittest.mock import patch
 
 
 class RecommendationCalculatorTest(unittest.TestCase):
-    @patch('requests.get')
-    def test_rating_is_based_on_top_track_playcount(self, mock_get):
+    def test_rating_is_based_on_top_track_playcount(self):
         scrobbled_track = ScrobbledTrack(artist='The Beatles', track_name='Penny Lane', playcount=10)
         recommended_track_1 = RecommendedTrack(artist='The Beatles', track_name='Strawberry Fields Forever')
         recommendations = {
