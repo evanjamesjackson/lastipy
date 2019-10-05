2a9f1808342ed3f361a66429a91ef4ee15fc1e77
Evan Jackson <evanjamesjackson@gmail.com>
2019-10-04 22:29:25 -0300

    renamed test

--- test/lastfm/test_recommendation_calculator.py
+++ test/lastfm/test_recommendation_calculator.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 
 class RecommendationCalculatorTest(unittest.TestCase):
     @patch('requests.get')
-    def test_recommendation_is_based_on_top_track_playcount(self, mock_get):
+    def test_rating_is_based_on_top_track_playcount(self, mock_get):
         scrobbled_track = ScrobbledTrack(artist='The Beatles', track_name='Penny Lane', playcount=10)
         recommended_track_1 = RecommendedTrack(artist='The Beatles', track_name='Strawberry Fields Forever')
         recommendations = {
