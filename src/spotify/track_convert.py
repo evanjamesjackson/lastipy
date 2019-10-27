6b8c2927722988ce5423c3fe399c5663455ae48e
Evan Jackson <evanjamesjackson@gmail.com>
2019-10-26 21:40:29 -0300

    Rearranged lines/comments so hopefully they're clearer

--- src/spotify/track_convert.py
+++ src/spotify/track_convert.py
@@ -6,11 +6,11 @@ def convert_json_tracks(json_tracks):
 
 
 def convert_json_track(json_track):
-    # Just getting the first artist, even if there's multiple
     if 'track' in json_track:
         # Some endpoints do this, others don't
         json_track = json_track['track']
-    artist = json_track['artists'][0]['name']
-    name = json_track['name']
     track_id = json_track['id']
+    name = json_track['name']
+    # Just getting the first artist, even if there's multiple
+    artist = json_track['artists'][0]['name']
     return SpotifyTrack(track_name=name, artist=artist, spotify_id=track_id)
