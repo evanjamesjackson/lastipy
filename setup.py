5d152f62901b5aac942e196643d5d64d10d62836
Evan Jackson <evanjamesjackson@gmail.com>
2020-03-15 12:23:33 -0300

    Added packages attribute back

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='spotify_recommender',
       version='1.0',
@@ -6,4 +6,5 @@ setup(name='spotify_recommender',
       url='http://github.com/evanjamesjackson/spotify_recommender',
       author='Evan Jackson',
       author_email='evanjamesjackson@gmail.com',
+      packages=find_packages(where='src'),
       install_requires=['numpy', 'requests', 'spotipy', 'pytest'])
