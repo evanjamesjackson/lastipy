44db82f25b1a9adb8cc0f46d50a3af7195bfe5df
Ubuntu <ubuntu@ip-172-31-40-114.us-east-2.compute.internal>
2020-03-17 01:37:49 +0000

    FIxed version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='spotify_recommender',
-      version='version="0.1.0"',
+      version='version="0.0.1"',
       description='Creates Spotify playlists based on your listening habits by pulling from sources like Last.fm.',
       url='http://github.com/evanjamesjackson/spotify_recommender',
       author='Evan Jackson',
