6380023a5ab5bd6157d4d6e1c5bcc2a493ac26f0
Jenkins <jenkins@ip-172-31-40-114.us-east-2.compute.internal>
2020-03-21 13:49:59 +0000

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='spotify_recommender',
-      version='0.1.9',
+      version='0.1.10',
       description='Creates Spotify playlists based on your listening habits by pulling from sources like Last.fm.',
       url='http://github.com/evanjamesjackson/spotify_recommender',
       author='Evan Jackson',
