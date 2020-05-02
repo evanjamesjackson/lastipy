c3979ce8425c7d31579d23e3b118e17d881d9883
Jenkins <jenkins@ip-172-31-40-114.us-east-2.compute.internal>
2020-05-02 16:27:09 +0000

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lastipy_recommender',
-      version='0.0.0',
+      version='0.0.1',
       description='Creates Spotify playlists based on your listening habits by pulling from sources like Last.fm.',
       url='http://github.com/evanjamesjackson/lastipy_recommender',
       author='Evan Jackson',
