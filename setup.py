480256432e48a3f23623bec9ed4434df672437a0
Evan Jackson <evanjamesjackson@gmail.com>
2020-12-27 18:09:13 -0400

    Manually setting version to 1.0.40

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lastipy',
-      version='1.0.0',
+      version='1.0.40',
       description='Python library that combines Last.fm and Spotify',
       url='http://github.com/evanjamesjackson/lastipy',
       author='Evan Jackson',
