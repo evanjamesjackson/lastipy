fed9db98e39c43d2d8473cf87cc55158c56741a1
Jenkins <jenkins@dedotated-wam>
2020-12-28 23:23:17 +0000

    Bump version: 1.0.48 → 1.0.49

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lastipy',
-      version='1.0.48',
+      version='1.0.49',
       description='Python library that combines Last.fm and Spotify',
       url='http://github.com/evanjamesjackson/lastipy',
       author='Evan Jackson',
