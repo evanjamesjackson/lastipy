1d77f070543aa8fbac07484bf2ceaf5464d3a804
Evan Jackson <evanjamesjackson@gmail.com>
2020-12-31 12:39:26 -0400

    In the process of extracing out some common functionality between endpoints

--- test/lastfm/library/test_recent_tracks.py
+++ test/lastfm/library/test_recent_tracks.py
@@ -11,126 +11,54 @@ class RecentArtistsTest(unittest.TestCase):
     dummy_user = 'dummyUser'
     dummy_api_key = '123456789'
 
-    @patch('requests.get')
-    def test_fetch_single_page(self, mock_requests_get):
+    @patch('lastipy.lastfm.library.recent_tracks.fetch')
+    def test_fetch(self, mock_paginated_endpoint_fetch):
         expected_tracks = [
             Track(track_name='Strawberry Fields Forever', artist='The Beatles'),
-            Track(track_name='Badge', artist='Cream')
+            Track(track_name='Badge', artist='Cream'),
+            Track(track_name='Black Dog', artist='Led Zeppelin')
         ]
 
-        mock_response = Mock()
-        mock_response.ok = True
-        mock_response.json.return_value = {
-            'recenttracks': {
-                'track': [
-                    {
-                        'name': 'Strawberry Fields Forever',
-                        'artist': {
-                            'name': 'The Beatles'
+        mock_paginated_endpoint_fetch.return_value = [
+            {
+                'recenttracks': {
+                    'track': [
+                        {
+                            'name': 'Strawberry Fields Forever',
+                            'artist': {
+                                'name': 'The Beatles'
+                            }
+                        },
+                        {
+                            'name': 'Badge',
+                            'artist': {
+                                'name': 'Cream'
+                            }
                         }
-                    },
-                    {
-                        'name': 'Badge',
-                        'artist': {
-                            'name': 'Cream'
-                        }
+                    ],
+                    '@attr': {
+                        'totalPages': '2'
                     }
-                ],
-                '@attr': {
-                    'totalPages': '1'
+                }
+            },
+            {
+                'recenttracks': {
+                    'track': [
+                        {
+                            'name': 'Black Dog',
+                            'artist': {
+                                'name': 'Led Zeppelin'
+                            }
+                        }
+                    ],
+                    '@attr': {
+                        'totalPages': '2'
+                    }
                 }
             }
-        }
-        mock_requests_get.side_effect = [mock_response]
-
-        fetched_tracks = recent_tracks.fetch_recent_tracks(
-            user=self.dummy_user, api_key=self.dummy_api_key)
-
-        self.assertCountEqual(fetched_tracks, expected_tracks)
-
-    @patch('requests.get')
-    def test_fetch_multiple_pages(self, mock_requests_get):
-        expected_tracks = [
-            Track(track_name='Stayin Alive', artist='Bee Gees'),
-            Track(track_name='Penny Lane', artist='The Beatles'),
-            Track(track_name='Badge', artist='Cream')
         ]
 
-        mock_responses = [Mock(), Mock(), Mock()]
-        mock_responses[0].ok = True
-        mock_responses[0].json.return_value = self._build_track_json_response(
-            'Stayin Alive', 'Bee Gees', '3')
-        mock_responses[1].ok = True
-        mock_responses[1].json.return_value = self._build_track_json_response(
-            'Penny Lane', 'The Beatles', '3')
-        mock_responses[2].ok = True
-        mock_responses[2].json.return_value = self._build_track_json_response(
-            'Badge', 'Cream', '3')
-        mock_requests_get.side_effect = mock_responses
-
         fetched_tracks = recent_tracks.fetch_recent_tracks(
             user=self.dummy_user, api_key=self.dummy_api_key)
 
         self.assertCountEqual(fetched_tracks, expected_tracks)
-
-    @patch('requests.get')
-    def test_fetch_with_success_after_retries(self, mock_requests_get):
-        expected_tracks = [
-            Track(track_name='Stayin Alive', artist='Bee Gees'),
-            Track(track_name='Penny Lane', artist='The Beatles'),
-        ]
-
-        mock_responses = [Mock(), Mock(), Mock()]
-        mock_responses[0].ok = True
-        mock_responses[0].json.return_value = self._build_track_json_response(
-            'Stayin Alive', 'Bee Gees', '2')
-        mock_responses[1].ok = False
-        mock_responses[1].raise_for_status.side_effect = HTTPError(
-            Mock(status=500), 'Error')
-        mock_responses[2].ok = True
-        mock_responses[2].json.return_value = self._build_track_json_response(
-            'Penny Lane', 'The Beatles', '2')
-        mock_requests_get.side_effect = mock_responses
-
-        fetched_tracks = recent_tracks.fetch_recent_tracks(
-            user=self.dummy_user, api_key=self.dummy_api_key)
-
-        self.assertCountEqual(fetched_tracks, expected_tracks)
-
-    @patch('requests.get')
-    def test_fetch_fails_after_retries(self, mock_requests_get):
-        mock_responses = []
-        for _ in range(10):
-            mock_response = Mock()
-            mock_response.ok = False
-            mock_response.raise_for_status.side_effect = HTTPError(
-                Mock(status=500), 'Error')
-            mock_responses.append(mock_response)
-
-        # Add another mock response, but the code will exit after the retry limit is reached and this won't actually get fetched
-        mock_response = Mock()
-        mock_response.ok = False
-        mock_response.json.return_value = self._build_track_json_response(
-            'Penny Lane', 'The Beatles', '1')
-
-        fetched_tracks = recent_tracks.fetch_recent_tracks(
-            user=self.dummy_user, api_key=self.dummy_api_key)
-
-        self.assertEqual(fetched_tracks, [])
-
-    def _build_track_json_response(self, name, artist_name, total_pages):
-        return {
-            'recenttracks': {
-                'track': [
-                    {
-                        'name': name,
-                        'artist': {
-                            'name': artist_name
-                        }
-                    }
-                ],
-                '@attr': {
-                    'totalPages': total_pages
-                }
-            }
-        }
--- test/lastfm/library/test_top_tracks.py
+++ test/lastfm/library/test_top_tracks.py
@@ -1,127 +1,127 @@
 
-import unittest
-from lastipy.lastfm.library import period
-from lastipy.lastfm.library.scrobbled_track import ScrobbledTrack
-from unittest.mock import patch, Mock
-from requests import HTTPError
-from lastipy.lastfm.library.top_tracks import fetch_top_tracks
+# import unittest
+# from lastipy.lastfm.library import period
+# from lastipy.lastfm.library.scrobbled_track import ScrobbledTrack
+# from unittest.mock import patch, Mock
+# from requests import HTTPError
+# from lastipy.lastfm.library.top_tracks import fetch_top_tracks
 
 
-class TopTracksFetcherTest(unittest.TestCase):
-    
-    @patch('requests.get')
-    def test_one_page_of_results(self, mock_requests_get):
-        expected_track = ScrobbledTrack(track_name="Stayin' Alive", artist="Bee Gees", playcount=2)
+# class TopTracksFetcherTest(unittest.TestCase):
 
-        mock_requests_get.ok = True
+#     @patch('requests.get')
+#     def test_one_page_of_results(self, mock_requests_get):
+#         expected_track = ScrobbledTrack(track_name="Stayin' Alive", artist="Bee Gees", playcount=2)
 
-        json_track = self._build_json(expected_track)
+#         mock_requests_get.ok = True
 
-        first_page_response = {
-            'toptracks': {
-                'track': [json_track]
-            }
-        }
+#         json_track = self._build_json(expected_track)
 
-        second_page_response = {
-            'toptracks': {
-                'track': []
-            }
-        }
+#         first_page_response = {
+#             'toptracks': {
+#                 'track': [json_track]
+#             }
+#         }
 
-        mock_responses = [Mock(), Mock()]
-        mock_responses[0].json.return_value = first_page_response
-        mock_responses[1].json.return_value = second_page_response
-        mock_requests_get.side_effect = mock_responses
+#         second_page_response = {
+#             'toptracks': {
+#                 'track': []
+#             }
+#         }
 
-        self.assertEqual(fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)[0], expected_track)
+#         mock_responses = [Mock(), Mock()]
+#         mock_responses[0].json.return_value = first_page_response
+#         mock_responses[1].json.return_value = second_page_response
+#         mock_requests_get.side_effect = mock_responses
 
-    @patch('requests.get')
-    def test_multiple_tracks_over_multiple_pages(self, mock_requests_get):
-        expected_track_1 = ScrobbledTrack(track_name="Penny Lane", artist="The Beatles", playcount=5)
-        expected_track_2 = ScrobbledTrack(track_name="Won't Get Fooled Again", artist="The Who", playcount=6)
-        expected_track_3 = ScrobbledTrack(track_name="Like the FBI", artist="Bob Dylan", playcount=10)
-        expected_tracks = [expected_track_1, expected_track_2, expected_track_3]
+#         self.assertEqual(fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)[0], expected_track)
 
-        mock_requests_get.ok = True
+#     @patch('requests.get')
+#     def test_multiple_tracks_over_multiple_pages(self, mock_requests_get):
+#         expected_track_1 = ScrobbledTrack(track_name="Penny Lane", artist="The Beatles", playcount=5)
+#         expected_track_2 = ScrobbledTrack(track_name="Won't Get Fooled Again", artist="The Who", playcount=6)
+#         expected_track_3 = ScrobbledTrack(track_name="Like the FBI", artist="Bob Dylan", playcount=10)
+#         expected_tracks = [expected_track_1, expected_track_2, expected_track_3]
 
-        json_track_1 = self._build_json(expected_track_1)
-        json_track_2 = self._build_json(expected_track_2)
-        json_track_3 = self._build_json(expected_track_3)
+#         mock_requests_get.ok = True
 
-        first_page_response = {
-            'toptracks': {
-                'track': [json_track_1, json_track_2]
-            }
-        }
+#         json_track_1 = self._build_json(expected_track_1)
+#         json_track_2 = self._build_json(expected_track_2)
+#         json_track_3 = self._build_json(expected_track_3)
 
-        second_page_response = {
-            'toptracks': {
-                'track': [json_track_3]
-            }
-        }
+#         first_page_response = {
+#             'toptracks': {
+#                 'track': [json_track_1, json_track_2]
+#             }
+#         }
 
-        third_page_response = {
-            'toptracks': {
-                'track': []
-            }
-        }
+#         second_page_response = {
+#             'toptracks': {
+#                 'track': [json_track_3]
+#             }
+#         }
 
-        mock_responses = [Mock(), Mock(), Mock()]
-        mock_responses[0].json.return_value = first_page_response
-        mock_responses[1].json.return_value = second_page_response
-        mock_responses[2].json.return_value = third_page_response
-        mock_requests_get.side_effect = mock_responses
+#         third_page_response = {
+#             'toptracks': {
+#                 'track': []
+#             }
+#         }
 
-        fetched_tracks = fetch_top_tracks(api_key='', user="sonofjack3", a_period=period.SEVEN_DAYS)
-        self.assertCountEqual(fetched_tracks, expected_tracks)
-    
-    @patch('requests.get')
-    def test_songs_with_one_playcount_ignored(self, mock_requests_get):
-        ignored_track_1 = ScrobbledTrack(track_name="Stayin' Alive", artist="Bee Gees", playcount=1)
-        non_ignored_track = ScrobbledTrack(track_name="Ventura Highway", artist="America", playcount=5)
-        ignored_track_2 = ScrobbledTrack(track_name="Anesthetized Lesson", artist="Gum", playcount=1)
+#         mock_responses = [Mock(), Mock(), Mock()]
+#         mock_responses[0].json.return_value = first_page_response
+#         mock_responses[1].json.return_value = second_page_response
+#         mock_responses[2].json.return_value = third_page_response
+#         mock_requests_get.side_effect = mock_responses
 
-        mock_requests_get.ok = True
+#         fetched_tracks = fetch_top_tracks(api_key='', user="sonofjack3", a_period=period.SEVEN_DAYS)
+#         self.assertCountEqual(fetched_tracks, expected_tracks)
 
-        json_track_1 = self._build_json(ignored_track_1)
-        json_track_2 = self._build_json(non_ignored_track)
-        json_track_3 = self._build_json(ignored_track_2)
+#     @patch('requests.get')
+#     def test_songs_with_one_playcount_ignored(self, mock_requests_get):
+#         ignored_track_1 = ScrobbledTrack(track_name="Stayin' Alive", artist="Bee Gees", playcount=1)
+#         non_ignored_track = ScrobbledTrack(track_name="Ventura Highway", artist="America", playcount=5)
+#         ignored_track_2 = ScrobbledTrack(track_name="Anesthetized Lesson", artist="Gum", playcount=1)
 
-        first_page_response = {
-            'toptracks': {
-                'track': [json_track_1, json_track_2, json_track_3]
-            }
-        }
+#         mock_requests_get.ok = True
 
-        second_page_response = {
-            'toptracks': {
-                'track': []
-            }
-        }
+#         json_track_1 = self._build_json(ignored_track_1)
+#         json_track_2 = self._build_json(non_ignored_track)
+#         json_track_3 = self._build_json(ignored_track_2)
 
-        mock_responses = [Mock(), Mock()]
-        mock_responses[0].json.return_value = first_page_response
-        mock_responses[1].json.return_value = second_page_response
-        mock_requests_get.side_effect = mock_responses
+#         first_page_response = {
+#             'toptracks': {
+#                 'track': [json_track_1, json_track_2, json_track_3]
+#             }
+#         }
 
-        fetched_tracks = fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)
-        self.assertEqual(fetched_tracks.__len__(), 1)
-        self.assertEqual(fetched_tracks[0], non_ignored_track)
-    
-    @patch('requests.get')
-    def test_failure(self, mock_requests_get):
-        mock_requests_get.ok = False
-        mock_requests_get.side_effect = HTTPError("Mock")
+#         second_page_response = {
+#             'toptracks': {
+#                 'track': []
+#             }
+#         }
 
-        with self.assertRaises(HTTPError):
-            fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)
+#         mock_responses = [Mock(), Mock()]
+#         mock_responses[0].json.return_value = first_page_response
+#         mock_responses[1].json.return_value = second_page_response
+#         mock_requests_get.side_effect = mock_responses
 
-    def _build_json(self, track):
-        return {
-            'name': track.track_name,
-            'artist': {
-                'name': track.artist
-            },
-            'playcount': track.playcount
-        }
+#         fetched_tracks = fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)
+#         self.assertEqual(fetched_tracks.__len__(), 1)
+#         self.assertEqual(fetched_tracks[0], non_ignored_track)
+
+#     @patch('requests.get')
+#     def test_failure(self, mock_requests_get):
+#         mock_requests_get.ok = False
+#         mock_requests_get.side_effect = HTTPError("Mock")
+
+#         with self.assertRaises(HTTPError):
+#             fetch_top_tracks(api_key='', user='sonofjack3', a_period=period.SEVEN_DAYS)
+
+#     def _build_json(self, track):
+#         return {
+#             'name': track.track_name,
+#             'artist': {
+#                 'name': track.artist
+#             },
+#             'playcount': track.playcount
+#         }
