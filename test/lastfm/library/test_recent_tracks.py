c65361145b7485d35b049c77fa506e9c1948e362
Evan Jackson <evanjamesjackson@gmail.com>
2020-12-30 21:18:45 -0400

    Formatting

--- test/lastfm/library/test_recent_tracks.py
+++ test/lastfm/library/test_recent_tracks.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from lastipy.lastfm.library import recent_tracks
 from unittest.mock import Mock
 from requests import HTTPError
-from lastipy.track import Track 
+from lastipy.track import Track
 
 
 class RecentArtistsTest(unittest.TestCase):
@@ -26,8 +26,8 @@ class RecentArtistsTest(unittest.TestCase):
                     {
                         'name': 'Strawberry Fields Forever',
                         'artist': {
-                            'name': 'The Beatles'  
-                        } 
+                            'name': 'The Beatles'
+                        }
                     },
                     {
                         'name': 'Badge',
@@ -43,10 +43,10 @@ class RecentArtistsTest(unittest.TestCase):
         }
         mock_requests_get.side_effect = [mock_response]
 
-        fetched_tracks = recent_tracks.fetch_recent_tracks(user=self.dummy_user, api_key=self.dummy_api_key)
+        fetched_tracks = recent_tracks.fetch_recent_tracks(
+            user=self.dummy_user, api_key=self.dummy_api_key)
 
         self.assertCountEqual(fetched_tracks, expected_tracks)
-
 
     @patch('requests.get')
     def test_fetch_multiple_pages(self, mock_requests_get):
@@ -58,17 +58,20 @@ class RecentArtistsTest(unittest.TestCase):
 
         mock_responses = [Mock(), Mock(), Mock()]
         mock_responses[0].ok = True
-        mock_responses[0].json.return_value = self._build_track_json_response('Stayin Alive', 'Bee Gees', '3')
+        mock_responses[0].json.return_value = self._build_track_json_response(
+            'Stayin Alive', 'Bee Gees', '3')
         mock_responses[1].ok = True
-        mock_responses[1].json.return_value = self._build_track_json_response('Penny Lane', 'The Beatles', '3')
+        mock_responses[1].json.return_value = self._build_track_json_response(
+            'Penny Lane', 'The Beatles', '3')
         mock_responses[2].ok = True
-        mock_responses[2].json.return_value = self._build_track_json_response('Badge', 'Cream', '3')
+        mock_responses[2].json.return_value = self._build_track_json_response(
+            'Badge', 'Cream', '3')
         mock_requests_get.side_effect = mock_responses
 
-        fetched_tracks = recent_tracks.fetch_recent_tracks(user=self.dummy_user, api_key=self.dummy_api_key)
+        fetched_tracks = recent_tracks.fetch_recent_tracks(
+            user=self.dummy_user, api_key=self.dummy_api_key)
 
         self.assertCountEqual(fetched_tracks, expected_tracks)
-
 
     @patch('requests.get')
     def test_fetch_with_success_after_retries(self, mock_requests_get):
@@ -79,17 +82,20 @@ class RecentArtistsTest(unittest.TestCase):
 
         mock_responses = [Mock(), Mock(), Mock()]
         mock_responses[0].ok = True
-        mock_responses[0].json.return_value = self._build_track_json_response('Stayin Alive', 'Bee Gees', '2')
+        mock_responses[0].json.return_value = self._build_track_json_response(
+            'Stayin Alive', 'Bee Gees', '2')
         mock_responses[1].ok = False
-        mock_responses[1].raise_for_status.side_effect = HTTPError(Mock(status=500), 'Error')
+        mock_responses[1].raise_for_status.side_effect = HTTPError(
+            Mock(status=500), 'Error')
         mock_responses[2].ok = True
-        mock_responses[2].json.return_value = self._build_track_json_response('Penny Lane', 'The Beatles', '2')
+        mock_responses[2].json.return_value = self._build_track_json_response(
+            'Penny Lane', 'The Beatles', '2')
         mock_requests_get.side_effect = mock_responses
 
-        fetched_tracks = recent_tracks.fetch_recent_tracks(user=self.dummy_user, api_key=self.dummy_api_key)
+        fetched_tracks = recent_tracks.fetch_recent_tracks(
+            user=self.dummy_user, api_key=self.dummy_api_key)
 
         self.assertCountEqual(fetched_tracks, expected_tracks)
-
 
     @patch('requests.get')
     def test_fetch_fails_after_retries(self, mock_requests_get):
@@ -97,32 +103,34 @@ class RecentArtistsTest(unittest.TestCase):
         for _ in range(10):
             mock_response = Mock()
             mock_response.ok = False
-            mock_response.raise_for_status.side_effect = HTTPError(Mock(status=500), 'Error')
+            mock_response.raise_for_status.side_effect = HTTPError(
+                Mock(status=500), 'Error')
             mock_responses.append(mock_response)
 
         # Add another mock response, but the code will exit after the retry limit is reached and this won't actually get fetched
         mock_response = Mock()
         mock_response.ok = False
-        mock_response.json.return_value = self._build_track_json_response('Penny Lane', 'The Beatles', '1')
+        mock_response.json.return_value = self._build_track_json_response(
+            'Penny Lane', 'The Beatles', '1')
 
-        fetched_tracks = recent_tracks.fetch_recent_tracks(user=self.dummy_user, api_key=self.dummy_api_key)
+        fetched_tracks = recent_tracks.fetch_recent_tracks(
+            user=self.dummy_user, api_key=self.dummy_api_key)
 
         self.assertEqual(fetched_tracks, [])
 
-
     def _build_track_json_response(self, name, artist_name, total_pages):
-        return { 
-            'recenttracks': { 
-                'track': [ 
-                    { 
-                        'name': name, 
-                        'artist': { 
-                            'name': artist_name 
-                        } 
+        return {
+            'recenttracks': {
+                'track': [
+                    {
+                        'name': name,
+                        'artist': {
+                            'name': artist_name
+                        }
                     }
-                ], 
-                '@attr': { 
-                    'totalPages': total_pages 
-                } 
-            } 
+                ],
+                '@attr': {
+                    'totalPages': total_pages
+                }
+            }
         }
