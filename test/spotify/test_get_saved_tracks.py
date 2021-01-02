5fcb0dde1d7ccca6730527a335dc71b79079695a
Evan Jackson <evanjamesjackson@gmail.com>
2021-01-02 12:49:18 -0400

    Asserting fetched tracks are correct

--- test/spotify/test_get_saved_tracks.py
+++ test/spotify/test_get_saved_tracks.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 from spotipy import Spotify
 from lastipy.spotify.library import get_saved_tracks
+from lastipy.spotify.spotify_track import SpotifyTrack
 
 
 class GetSavedTracksTest(unittest.TestCase):
@@ -11,6 +12,10 @@ class GetSavedTracksTest(unittest.TestCase):
         self.mock_spotify.current_user = MagicMock({'id': 'dummyUser'})
 
     def test_fetch_single_page(self):
+        expected_saved_tracks = [SpotifyTrack(
+            track_name='Penny Lane', artist='The Beatles', spotify_id='123456789')
+        ]
+
         self.mock_spotify.current_user_saved_tracks = MagicMock()
         mock_saved_tracks_response = {
             'items': [{
@@ -28,4 +33,6 @@ class GetSavedTracksTest(unittest.TestCase):
         self.mock_spotify.current_user_saved_tracks.side_effect = [
             mock_saved_tracks_response, {'items': []}]
 
-        get_saved_tracks(self.mock_spotify)
+        fetched_saved_tracks = get_saved_tracks(self.mock_spotify)
+
+        self.assertCountEqual(fetched_saved_tracks, expected_saved_tracks)
