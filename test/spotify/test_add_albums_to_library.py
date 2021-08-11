6fcde6ea69ab6817b33708e0c8839c47a4c2fb37
Evan Jackson <evan@jacksonnet.ca>
2021-08-11 11:26:56 -0300

    Forcing a test to fail to see if status updates in GitHub

--- test/spotify/test_add_albums_to_library.py
+++ test/spotify/test_add_albums_to_library.py
@@ -30,7 +30,7 @@ class AddAlbumsToLibraryTest(unittest.TestCase):
         for _ in range(3):
             chunk = []
             for _ in range(50):
-                chunk.append("123")
+                chunk.append("1234")
             expected_chunks.append(chunk)
 
         add_albums_to_library(spotify, dummy_albums)
